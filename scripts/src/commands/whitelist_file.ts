d65370af58b489996d7dc14cc7892752baea1203
Abdelhamid Bakhta <abdelhamid.bakhta@gmail.com>
2022-05-04 16:24:22 +0200

    feat: add whitelist through JSON file

---
+++ scripts/src/commands/whitelist_file.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import * as readline from 'node:readline';
+import { stdin, stdout } from 'process';
+import {question} from 'readline-sync';
+import { Keychain } from '../core/keychain';
+import { logger } from '../core/logger';
+import {load} from "../core/core";
+import {config} from "../config";
+import {readFileSync} from 'fs';
+
+const rl = readline.createInterface({
+    input: stdin,
+    output: stdout
+});
+
+
+async function connect() {
+    const { keychain, contracts, nftContract, sellContract, alreadyUploaded } = await load();
+    if (!alreadyUploaded) {
+        return ;
+    }
+    const whitelistFilePath = question('Whitelist file path: ');
+    logger.info('Reading whitelist file: ', whitelistFilePath);
+    const rawWhitelist = readFileSync(whitelistFilePath);
+    const whitelist = JSON.parse(rawWhitelist.toString());
+    await sellContract.executeAddToWhitelist(Keychain.ADMIN, whitelist);
+}
+
+connect().then(() => {
+    logger.info('done');
+})
