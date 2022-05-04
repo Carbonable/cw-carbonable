65d0beec7d2f4f970be9fa5522d75fe937b63378
Abdelhamid Bakhta <abdelhamid.bakhta@gmail.com>
2022-05-04 16:27:26 +0200

    remove testnet config

--- scripts/src/config.ts
+++ scripts/src/config.ts
@@ -30,31 +30,6 @@ export let config: ObjAny = {
     anonymous_mnemonic: process.env.ANON_MNEMONIC
 };
 
-export let config2: ObjAny = {
-    // chain config
-    denom: env.DENOM || 'upebble',
-    rpcServer: env.RPC_SERVER || 'https://rpc.cliffnet.cosmwasm.com',
-    prefix: env.CHAIN_PREFIX || 'wasm',
-    needFaucet: true,
-    faucet: env.FAUCET_SERVER || 'https://faucet.cliffnet.cosmwasm.com',
-
-    // contracts config
-    instantiateConfigPath: './contract.config',
-    contractPath: env.CONTRACT_PATH || './artifacts',
-    gasPrice: env.GAS_PRICE || '0.025',
-
-    // nft contract config
-    nftName: 'carbonable',
-    symbol: 'CARB',
-    collectionName: 'carbonable',
-    description: 'utility NFT',
-
-    // sell config
-    sellPrice: 100,
-    sellReservedAmount: 100,
-    sellMarketAmount: 9900,
-};
-
 export function updateConfig(otherConfig: ObjAny) {
     config = {...config, ...otherConfig};
 }
